0334e671f810590457dd4b3ba20829dd9ae0376d
Bastion <sebastianvidalsalvarredi@gmail.com>
2026-06-01 20:10:12 -0300

    Estructura SPA completada, imágenes configuradas y TODO list agregado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Las importaciones solo llevan el nombre del componente y su ubicación
+import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { Registro } from './pages/registro/registro';
+import { Tienda } from './pages/tienda/tienda';
+import { Admin } from './pages/admin/admin';
+
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'login', component: Login },
+  { path: 'registro', component: Registro },
+  { path: 'tienda', component: Tienda },
+  { path: 'admin', component: Admin },
+  { path: '**', redirectTo: '' } // Redirige al inicio si la URL no existe
+];
